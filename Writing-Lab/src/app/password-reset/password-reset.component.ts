3c0beb614544dfab8d1909f56c7d57ca42d296db
Rahima Mokter <rahimamokter@gmail.com>
2020-11-21 00:13:56 -0500

    Upto Individual Project report completed

---
+++ Writing-Lab/src/app/password-reset/password-reset.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../shared/services/auth.service";
+
+@Component({
+  selector: 'app-password-reset',
+  templateUrl: './password-reset.component.html',
+  styleUrls: ['./password-reset.component.css']
+})
+export class PasswordResetComponent implements OnInit {
+
+  constructor(
+    public authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
